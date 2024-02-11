78521ed7b320fe80fb1aeabc7d24dd07888e5427
khuk <Kostiantyn.Huk@teck.com>
2024-02-10 21:43:20 -0700

    changed behaivior of letter

--- src/Scenes/Alphabet/FindMissing.jsx
+++ src/Scenes/Alphabet/FindMissing.jsx
@@ -1,59 +1,25 @@
 import {alphabet, getNumber} from "../../shared/alphabet";
 import {Box, Button, Grid, Input} from "@mui/material";
-import React, {useEffect, useState} from "react";
-import {useSharedDispatch} from "../../store/store";
-import {correctAction} from "../../store/actions/scoreActions";
+import React, {useState} from "react";
 import Letter from "../../components/Letter";
 
 const FindMissing = () => {
-    const [selected, setSelected] = useState('');
-    const [pressed, setPressed] = useState(false);
-    const [_alphabet, setAlphabet] = useState([...alphabet])
     const [missingIndex, setMissingIndex] = useState(getNumber());
-    const dispatch = useSharedDispatch();
-    const missing = _alphabet[missingIndex];
-    let __alphabet = [..._alphabet];
-    __alphabet[missingIndex] = '';
 
-    const onInput = (event) => {
-        const selected = event.target.value.toUpperCase();
-        setSelected(selected);
-    };
-    const onCheck = () => {
-        const isCorrect = missing === selected;
-        setPressed(true);
-        if(isCorrect) {
-            setAlphabet([...alphabet]);
-            dispatch(correctAction)
-        }
-        setTimeout(() => {
-            setPressed(false);
-            if (isCorrect) {
-                setMissingIndex(getNumber());
-                setSelected('')
-            }
-        }, 1000)
-    };
-    const sx = {};
-    if (pressed) {
-        sx.backgroundColor = missing === selected ? 'green' : 'red';
-        if (missing === selected) {
-            __alphabet = [...alphabet]
-        }
+    const onSuccess = () => {
+        setMissingIndex(getNumber());
     }
   return (
       <Box>
           <Grid container direction={'row'}>
-              {__alphabet.map((letter, i) => {
+              {alphabet.map((letter, i) => {
                   return (
                       <Grid item key={'FM'+letter}>
-                          <Letter letter={letter} />
+                          <Letter letter={letter} missingIndex={missingIndex} index={i} onSuccess={onSuccess}/>
                       </Grid>
                   )
               })}
           </Grid>
-          <Input inputProps={{maxLength: 1}} variant={'contained'} value={selected} sx={sx} onChange={onInput} />
-          <Button variant={'contained'} onClick={onCheck}>Check</Button>
       </Box>
   )
 }
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,22 +1,74 @@
-import {Box} from "@mui/material";
+import {Box, Input} from "@mui/material";
+import React, {useEffect, useMemo, useRef, useState} from "react";
+import {correctAction} from "../store/actions/scoreActions";
+import {useSharedDispatch} from "../store/store";
 
-const Letter = ({letter}) => {
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+const Letter = ({letter, missingIndex, index, onSuccess}) => {
+    const dispatch = useSharedDispatch();
+    const [selected, setSelected] = useState('');
+
+    const rotation = useMemo(() => randomIntFromInterval(-20, 20), [missingIndex])
+
+    const isMissing = missingIndex === index;
+
+    const onInput = (event) => {
+        const selected = event.target.value.toUpperCase();
+        setSelected(selected);
+        const isCorrect = letter === selected;
+        if(isCorrect) {
+            let utterance = new SpeechSynthesisUtterance("Correct!");
+            speechSynthesis.speak(utterance);
+            utterance = new SpeechSynthesisUtterance(letter.toLowerCase());
+            speechSynthesis.speak(utterance);
+            dispatch(correctAction);
+        } else {
+            let utterance = new SpeechSynthesisUtterance('Oh No!!! Wrong.');
+            speechSynthesis.speak(utterance);
+        }
+        setTimeout(() => {
+            setSelected('');
+            if (isCorrect) onSuccess();
+        }, 1000)
+    };
+
     const onLetterClick = () => {
+        if (isMissing) return;
         let utterance = new SpeechSynthesisUtterance(letter.toLowerCase());
         speechSynthesis.speak(utterance);
     }
+
+    const sx = {};
+    if (selected) {
+        sx.backgroundColor = letter === selected ? 'green' : 'red';
+    }
+
     return (
         <Box sx={{
+            ...sx,
             cursor: 'pointer',
                 width: '60px',
                 height: '60px',
+                borderRadius: '8px',
                 border: '1px solid black',
                 textAlign: 'center',
                 paddingTop: '5px',
+                userSelect: 'none',
+                transform: `rotate(${rotation}deg)`,
+                transition: `transform 1s`
             }}
              onClick={onLetterClick}
         >
-            {letter}
+            {missingIndex === index ? (
+                <Input sx={{
+                    fontSize: '40px',
+                    height: '100%',
+                    paddingLeft: '15px'
+                }} inputProps={{maxLength: 1}} variant={'contained'} value={selected} onChange={onInput} />
+            ) : letter }
+
         </Box>
     )
 }
